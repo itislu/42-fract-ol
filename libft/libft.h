cb99f89af1faeafc35ecbaec8c9c57a63e5ac09e
Lukas Dullinger <ldulling@c1r1p4.42vienna.com>
2023-11-12 11:19:53 +0100

    add ft_strcmp to libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:17:46 by ldulling          #+#    #+#             */
-/*   Updated: 2023/09/24 16:44:34 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/11/12 10:30:15 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,5 +71,8 @@ t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
+
+/* New functions */
+int		ft_strcmp(const char *s1, const char *s2);
 
 #endif
