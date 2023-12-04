a37ef70783111ee49dbe8a1d983762f792dc8df3
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2023-12-04 13:27:22 +0100

    feat: Align ft_memcpy and ft_strlcat segfault behaviour to original functions + add documentation

--- libft/src/libft/memory/ft_memcpy.c
+++ libft/src/libft/memory/ft_memcpy.c
@@ -6,9 +6,24 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:08:20 by ldulling          #+#    #+#             */
-/*   Updated: 2023/09/25 15:51:02 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/12/04 11:49:22 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/**
+ * The ft_memcpy function copies n bytes from memory area src to memory area
+ * dest.
+ * The memory areas must not overlap.
+ *
+ * @param dest  The memory area to copy to.
+ * @param src   The memory area to copy from.
+ * @param n     The number of bytes to copy.
+ *              Note: Giving dest or src as NULL with n not 0 will cause a
+ *              segmentation fault.
+ *
+ * @return Returns a pointer to dest.
+ *
+ */
 
 #include "libft.h"
 
@@ -18,7 +33,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*casted_ptr_src;
 	size_t			i;
 
-	if (dest || src)
+	if (n)
 	{
 		casted_ptr_dest = (unsigned char *) dest;
 		casted_ptr_src = (unsigned char *) src;
--- libft/src/libft/strings/ft_strlcat.c
+++ libft/src/libft/strings/ft_strlcat.c
@@ -6,9 +6,29 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:15:16 by ldulling          #+#    #+#             */
-/*   Updated: 2023/09/25 17:15:46 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/12/04 12:31:10 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/**
+ * The ft_strlcat function appends the NUL-terminated string src to the end of
+ * dst.
+ * It will append at most size - strlen(dst) - 1 characters.
+ * It will then NUL-terminate, unless size is 0 or the original dst string was
+ * longer than size.
+ *
+ * @param dst   The string to be appended to.
+ *              Note: Giving dst as NULL with a size not 0 will cause a
+ *              segmentation fault.
+ * @param src   The string to append to dst.
+ *              Note: Giving src as NULL will cause a segmentation fault.
+ * @param size  The total size of dst, including the space for the
+ *              NUL-terminator.
+ *
+ * @return The total length of the string it tried to create, that means the
+ *         initial length of dst plus the length of src.
+ *         If the return value is >= size, the output string has been truncated.
+ */
 
 #include "libft.h"
 
@@ -18,7 +38,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	dstlen;
 	size_t	srclen;
 
-	if (dst == NULL && size == 0)
+	if (dst == NULL && size == 0 && src)
 		return (0);
 	dstlen = ft_strlen(dst);
 	catlen = dstlen;
